eb8298a5b2f3008c28d1dc243e96fd97ce710bc0
unknown <shirley_loayza_sanchez@brown.edu>
2021-11-21 14:44:05 -0500

    preprocessing part 2

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -1,4 +1,29 @@
 import numpy as np
 import pandas as pd
-path = "../data/train-v1.1.json"
-data = pd.read_json(path)
+import json
+path = "./data/train-v1.1.json"
+with open(path, 'r') as datafile:
+    data = json.load(datafile)['data']
+#data is a list with data points - 442 articles
+#paragraphs (list) -> (context, **qas) (list) -> (**answers, questions, id) (list) ->
+#(answer start, text) 
+processed_questions = []
+processed_answers = []
+count = 0
+for paragraph in data:
+    paragraph_list = paragraph['paragraphs']
+    for qas in paragraph_list:
+        qas_list = qas['qas']
+        for qa in qas_list:
+            question = qa['question']
+            answer = qa['answers'][0]['text']
+            processed_questions.append(question)
+            processed_answers.append(answer)
+            count += 1
+            if (count%100 == 0):
+                print("Data point #", count)
+print(count)
+print(processed_questions[:10])
+print(processed_answers[:10])
+print(processed_questions[-10:])
+print(processed_answers[-10:])
