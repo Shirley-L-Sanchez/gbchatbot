9b20673dbe0e875cc144e5ee2a09412d73546a3d
cfiore-jpg <77872473+cfiore-jpg@users.noreply.github.com>
2021-12-07 22:15:39 -0500

    Update gbchatbot(debugging).py

--- code/gbchatbot(debugging).py
+++ code/gbchatbot(debugging).py
@@ -227,7 +227,7 @@ gan.build(input_shape=(z_dim))
 gan.compile(optimizer=Adam(learning_rate=0.001),loss='binary_crossentropy',metrics=['accuracy'])
 #seq2seq
 discriminator_supervised = discriminator_supervised(shared_layers)
-discriminator_supervised = build(input_shape=(768))
+discriminator_supervised = discriminator_supervised.build(input_shape=(768))
 
 from transformers import BertTokenizer, BertModel
 tokenizer = BertTokenizer.from_pretrained('distilbert-base-uncased')
